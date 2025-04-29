48616af0ccfa9542e7b17689148163849cfe11f5
Aditya Dhanraj <sanusinha65@gmail.com>
2025-04-29 15:17:32 +0530

    Changes

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use('*all', async (req, res) => {
       render = (await vite.ssrLoadModule('/src/entry-server.jsx')).render
     } else {
       template = templateHtml
-      render = (await import('./dist/server/entry-server.js')).render
+      render = (await import('./dist/server/entry-server.jsx')).render
     }
 
     const rendered = await render(url)
